65b125541684816aa1112f4739f14a4a7271c064
嘿鹰 <hhhhhy1747@outlook.com>
2025-04-21 13:07:19 +0800

    fix(bukkit): 使用 UUID.nameUUIDFromBytes 对头颅纹理生成 UUID

--- module/bukkit/bukkit-util/src/main/kotlin/taboolib/platform/util/BukkitSkull.kt
+++ module/bukkit/bukkit-util/src/main/kotlin/taboolib/platform/util/BukkitSkull.kt
@@ -140,7 +140,7 @@ object BukkitSkull {
         } else {
             // 如果使用 1.18.1 及以下版本, 则使用老方法处理
             val texture = if (headBase64.length in 60..100) encodeTexture(headBase64) else headBase64
-            val profile = GameProfile(UUID.nameUUIDFromBytes(texture.decodeBase64()), "TabooLib")
+            val profile = GameProfile(UUID.nameUUIDFromBytes(texture.toByteArray()), "TabooLib")
             profile.properties.put("textures", Property("textures", texture, "TabooLib_TexturedSkull"))
 
             meta.setProperty("profile", profile)
